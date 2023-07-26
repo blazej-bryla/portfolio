5bc3701f30a5b1eb8eff5cece463b5e81978b34d
blazej-bryla <brykuski@gmail.com>
2023-07-26 14:47:47 +0200

    feat: create simply mobiel navbar

---
+++ app/components/Navbar/MobileNavbar/MobileNavbar.tsx
@@ -0,0 +1,15 @@
+import { MenuItems } from '@/app/constants'
+import React from 'react'
+import { MobileMenuItem } from '..'
+
+export const MobileNavbar = () => {
+  return (
+    <div className="absolute bottom-0 flex h-[100px] w-full items-center justify-center bg-dark px-2 lg:sr-only">
+      <div className="flex w-full items-center justify-between">
+        {MenuItems.map((item) => (
+          <MobileMenuItem key={`${item.label} - menu`} item={item} />
+        ))}
+      </div>
+    </div>
+  )
+}
