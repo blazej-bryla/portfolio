5df625c10482d50cd65bd4692b7fbf590713d66e
blazej-bryla <brykuski@gmail.com>
2023-07-31 09:08:06 +0200

    fix: position fixed to mobile navbar component

--- app/components/Navbar/MobileNavbar/MobileNavbar.tsx
+++ app/components/Navbar/MobileNavbar/MobileNavbar.tsx
@@ -4,7 +4,7 @@ import { MobileMenuItem } from '..'
 
 export const MobileNavbar = () => {
   return (
-    <div className="absolute bottom-0 flex h-[100px] w-full items-center justify-center bg-dark px-2 md:sr-only">
+    <div className="fixed bottom-0 flex h-[100px] w-full items-center justify-center bg-dark px-2 md:sr-only">
       <div className="flex w-full items-center justify-evenly">
         {MenuItems.map((item) => (
           <MobileMenuItem key={`${item.label} - menu`} item={item} />
