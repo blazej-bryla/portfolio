88313e9ea96300df790de05b4a1aee02e0b3a336
blazej-bryla <brykuski@gmail.com>
2023-08-01 10:11:42 +0200

    refactor: rebuild technologies list

--- app/pages/TechnologyStack.tsx
+++ app/pages/TechnologyStack.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, SectionLabel } from '../components'
-import { TechnologiesList } from '../constants/TechnologiesItems'
 import { TechnologiesCategory } from './TechnologyStack/TechnologiesCategory'
+import { TechnologiesCategoryList } from '../constants'
 
 export const TechnologyStack = () => {
   return (
@@ -9,7 +9,7 @@ export const TechnologyStack = () => {
       <Container>
         <SectionLabel label="My Technology Stack:" />
         <div className="flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-4">
-          {TechnologiesList.map((item, i) => (
+          {TechnologiesCategoryList.map((item, i) => (
             <TechnologiesCategory item={item} key={i} />
           ))}
         </div>
