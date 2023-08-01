9adcc6f269f5aba6b1be761777f7f8b5dffe1417
blazej-bryla <brykuski@gmail.com>
2023-08-01 09:56:27 +0200

    feat: create base file structure

---
+++ app/pages/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, SectionLabel } from '../components'
+
+export const Projects = () => {
+  return (
+    <div className="bg-dark pt-24">
+      <Container>
+        <SectionLabel label="My Projects:" />
+      </Container>
+    </div>
+  )
+}
