f6046db53044b458b4ce3c454d04efbc87f612c7
blazej-bryla <brykuski@gmail.com>
2023-08-05 07:44:51 +0200

    feat: add simply icons to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Container } from '.'
+import { SocialMedia } from './SocialMedia'
+import { SocialItems } from '../constants'
 
 export const Footer = () => {
-  return <footer className="bg-white dark:bg-dark"></footer>
+  return (
+    <footer className="bg-white py-8 dark:bg-dark">
+      <Container>
+        <SocialMedia socialMediaItems={SocialItems} />
+      </Container>
+    </footer>
+  )
 }
