f178224eb58a94eebbd1ccb00e41f71009be8a46
blazej-bryla <brykuski@gmail.com>
2023-08-07 09:25:50 +0200

    feat: added original certificates and created function for automate gallery

--- app/constants/Certificates.ts
+++ app/constants/Certificates.ts
@@ -1,16 +1,23 @@
+import fs from 'fs'
+import path from 'path'
 import { GalleryProps } from '../types'
 
-export const CertificateList: Array<GalleryProps> = [
-  {
-    image: '/certificates/example-certificate.webp',
-    label: 'Example Certificate 1'
-  },
-  {
-    image: '/certificates/example-certificate.webp',
-    label: 'Example Certificate 1'
-  },
-  {
-    image: '/certificates/example-certificate.webp',
-    label: 'Example Certificate 1'
+const certificatesPath = path.join(__dirname, '../../../public/certificates')
+
+function getCertificateList(): Array<GalleryProps> {
+  try {
+    const files = fs.readdirSync(certificatesPath)
+    const certificateList: Array<GalleryProps> = files.map((filename) => {
+      return {
+        image: `/certificates/${filename}`,
+        label: filename.split('.')[0]
+      }
+    })
+    return certificateList
+  } catch (error) {
+    console.error('Error reading certificate files: ', error)
+    return []
   }
-]
+}
+
+export const CertificateList: Array<GalleryProps> = getCertificateList()
