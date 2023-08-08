a599a526be79c944ad5690248a4b3744a930a403
blazej-bryla <brykuski@gmail.com>
2023-08-08 09:20:41 +0200

    feat: add tooltip

--- app/components/TechnologiesItem.tsx
+++ app/components/TechnologiesItem.tsx
@@ -2,11 +2,14 @@
 import { TechnologiesItem as TechnologiesItemType } from '@/app/types'
 import { Icon } from '@iconify/react/dist/iconify.js'
 import React from 'react'
+import { MyTooltip } from '.'
 
 export const TechnologiesItem = ({ item }: { item: TechnologiesItemType }) => {
   return (
     <a href={item.href}>
-      <Icon icon={item.icon} width={40} height={40} />
+      <MyTooltip content={item.label}>
+        <Icon icon={item.icon} width={40} height={40} />
+      </MyTooltip>
     </a>
   )
 }
