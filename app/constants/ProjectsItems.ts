ea17a8b9ba46122bda68614b0bbccb901ea7b146
blazej-bryla <brykuski@gmail.com>
2023-08-08 15:54:58 +0200

    fix: project date

--- app/constants/ProjectsItems.ts
+++ app/constants/ProjectsItems.ts
@@ -4,7 +4,7 @@ import { ExpressJS, JavaScript, NodeJS } from './technologies'
 export const ProjectsItems: ProjectItem[] = [
   {
     label: 'Todo List API',
-    date: ' 2020',
+    date: ' 2022',
     technologies: [JavaScript, NodeJS, ExpressJS],
     description:
       'My application is a simple yet powerful Todo-List that enables easy management of daily tasks. You can add, delete, and mark tasks as completed. Built using JavaScript and Node.js with the Express framework, the app includes Jest-based testing and continuous integration with CircleCI. With a user-friendly interface and responsive design, it provides efficient task list management on any device.',
