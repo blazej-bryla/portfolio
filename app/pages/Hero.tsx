48e43eb3edc24a135b2dd8173b14052d94cb78f2
blazej-bryla <brykuski@gmail.com>
2023-08-10 10:54:39 +0200

    fix: fix themeswitcher position

--- app/pages/Hero.tsx
+++ app/pages/Hero.tsx
@@ -8,7 +8,7 @@ import { MemojiLaptop } from '../assets'
 export const Hero = () => {
   return (
     <div className="relative flex h-screen flex-col items-center justify-center">
-      <div className="absolute right-4 top-4 md:sr-only">
+      <div className="absolute right-4 top-4 z-10 md:sr-only">
         <ThemeSwitcher />
       </div>
       <div className=" flex -translate-y-[100px] flex-col items-center justify-center gap-8">
