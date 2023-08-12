4a3967b20ed0e1e7fa42980bc01dfd76218c2540
blazej-bryla <brykuski@gmail.com>
2023-08-12 17:55:43 +0200

    fix: change twitter links, fix icons tooltip"

--- app/components/SocialMedia/SocialMediaItem.tsx
+++ app/components/SocialMedia/SocialMediaItem.tsx
@@ -5,10 +5,10 @@ import { MyTooltip } from '..'
 
 export const SocialMediaItem = ({ item }: { item: SocialItemsType }) => {
   return (
-    <a href={item.href} title={item.label}>
-      <MyTooltip content={item.label}>
+    <MyTooltip content={item.label}>
+      <a href={item.href} title={item.label}>
         <Icon icon={item.icon} width={25} height={25} />
-      </MyTooltip>
-    </a>
+      </a>
+    </MyTooltip>
   )
 }
